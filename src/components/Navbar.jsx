7b6d52f7fda913fa990fe1e9e55b6a45051364f4
Alvaro U <aulian@ceibal.edu.uy>
2022-08-25 21:29:11 -0300

    Update, se agrega Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Gringos from "../assets/Gringos.png"
 
 function NavbarGringo() {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#home">Gringolandia</Navbar.Brand>
+        <img
+          src={Gringos}
+          width="80"
+          className="d-inline-block align-top"
+          alt="Gringos logo"
+        />
         <Nav className="me-auto">
           <Nav.Link href="#home">Inicio</Nav.Link>
           <Nav.Link href="#catalog">Catálogo</Nav.Link>
